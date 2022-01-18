03612284aa8d4b420a81f277aaa2c31ca4554e58
Izza303 <izzamariam047@gmail.com>
2022-01-18 14:36:08 +0500

    Izza Commit

--- app.js
+++ app.js
@@ -143,6 +143,42 @@ app.get("/forgot",sessionChecker, (req, res) => {
   
 });
 
+// app.get("/pagination", async (req, res, next) => {
+  
+//   try {
+//       let { page, size, sort } = req.query;
+
+//       // If the page is not applied in query.
+//       if (!page) {
+
+//           // Make the Default value one.
+//           page = 1;
+//       }
+
+//       if (!size) {
+//           size = 10;
+//       }
+
+//       //  We have to make it integer because
+//       // query parameter passed is string
+//       const limit = parseInt(size);
+
+//       // We pass 1 for sorting data in 
+//       // ascending order using ids
+//       const user = await User.find().sort(
+//           { votes: 1, _id: 1 }).limit(limit)
+//       res.send({
+//           page,
+//           size,
+//           Info: user,
+//       });
+//   }
+//   catch (error) {
+//       res.sendStatus(500);
+//   }
+// });
+
+
 app.post("/update",sessionChecker,(req, res) => {
   var email = req.body.email;
   counter=0;
