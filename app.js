b748217ed9b9a668782cd68736a30696eaa6f59f
Izza303 <izzamariam047@gmail.com>
2022-01-18 14:39:59 +0500

    Izza

--- app.js
+++ app.js
@@ -143,40 +143,40 @@ app.get("/forgot",sessionChecker, (req, res) => {
   
 });
 
-// app.get("/pagination", async (req, res, next) => {
+// // app.get("/pagination", async (req, res, next) => {
   
-//   try {
-//       let { page, size, sort } = req.query;
+// //   try {
+// //       let { page, size, sort } = req.query;
 
-//       // If the page is not applied in query.
-//       if (!page) {
+// //       // If the page is not applied in query.
+// //       if (!page) {
 
-//           // Make the Default value one.
-//           page = 1;
-//       }
+// //           // Make the Default value one.
+// //           page = 1;
+// //       }
 
-//       if (!size) {
-//           size = 10;
-//       }
+// //       if (!size) {
+// //           size = 10;
+// //       }
 
-//       //  We have to make it integer because
-//       // query parameter passed is string
-//       const limit = parseInt(size);
+// //       //  We have to make it integer because
+// //       // query parameter passed is string
+// //       const limit = parseInt(size);
 
-//       // We pass 1 for sorting data in 
-//       // ascending order using ids
-//       const user = await User.find().sort(
-//           { votes: 1, _id: 1 }).limit(limit)
-//       res.send({
-//           page,
-//           size,
-//           Info: user,
-//       });
-//   }
-//   catch (error) {
-//       res.sendStatus(500);
-//   }
-// });
+// //       // We pass 1 for sorting data in 
+// //       // ascending order using ids
+// //       const user = await User.find().sort(
+// //           { votes: 1, _id: 1 }).limit(limit)
+// //       res.send({
+// //           page,
+// //           size,
+// //           Info: user,
+// //       });
+// //   }
+// //   catch (error) {
+// //       res.sendStatus(500);
+// //   }
+// // });
 
 
 app.post("/update",sessionChecker,(req, res) => {
